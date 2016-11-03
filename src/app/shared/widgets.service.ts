28fc7dab58bca25320516d0a7388ea9264e1fdb8
Jonathan Garvey <jon.d.garvey@gmail.com>
2016-11-02 17:27:47 -0700

    remove unnecessary img url

--- src/app/shared/widgets.service.ts
+++ src/app/shared/widgets.service.ts
@@ -5,7 +5,6 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
 
 const BASE_URL = 'http://localhost:3000/widgets/';
-const IMG_URL = 'assets/img/';
 const HEADER = { headers: new Headers({ 'Content-Type': 'application/json' }) };
 
 @Injectable()
