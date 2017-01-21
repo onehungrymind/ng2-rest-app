d4cd2c649adb6d27bd5a25c81c4dfccfaab7503d
Jonathan Garvey <jon.d.garvey@gmail.com>
2017-01-20 17:40:44 -0700

    update to latest everything
    (cherry picked from commit a211c04)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,6 @@ import { FeaturedItemComponent } from './items/featured-item/featured-item.compo
 const routes: Routes = [
   {path: '',      redirectTo: '/items', pathMatch: 'full' },
   {path: 'items', component: ItemsComponent, children: [
-    {path: ''},
     {path: 'featured/:id', component: FeaturedItemComponent}
   ]},
   {path: '**',     redirectTo: '/items', pathMatch: 'full'}
