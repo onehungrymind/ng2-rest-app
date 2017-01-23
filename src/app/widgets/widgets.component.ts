8b37710372bd0b6072f650a1576a412b6488076a
Jonathan Garvey <jon.d.garvey@gmail.com>
2017-01-23 14:26:54 -0700

    fix route snapshot bug

--- src/app/widgets/widgets.component.ts
+++ src/app/widgets/widgets.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { WidgetsService, Widget } from '../shared';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-widgets',
@@ -11,7 +12,9 @@ export class WidgetsComponent implements OnInit {
   selectedWidget: Widget;
 
   constructor(
-    private widgetsService: WidgetsService
+    private widgetsService: WidgetsService,
+    private router: Router,
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit() {
@@ -27,6 +30,30 @@ export class WidgetsComponent implements OnInit {
     this.selectedWidget = widget;
   }
 
+  saveWidget(widget: Widget) {
+    const responseWidget = this.widgetsService.saveWidget(widget);
+
+    if (widget.id) {
+      this.replaceWidget(responseWidget);
+    } else {
+      this.pushWidget(responseWidget);
+    }
+
+    // Generally, we would want to wait for the result of `widgetsService.saveWidget`
+    // before resetting the current widget.
+    this.resetWidget();
+  }
+
+  replaceWidget(widget: Widget) {
+    this.widgets = this.widgets.map(mapWidget => {
+      return mapWidget.id === widget.id ? widget : mapWidget;
+    });
+  }
+
+  pushWidget(widget: Widget) {
+    this.widgets.push(widget);
+  }
+
   deleteWidget(widget: Widget) {
     this.widgetsService.deleteWidget(widget);
     this.widgets.splice(this.widgets.indexOf(widget), 1);
@@ -34,6 +61,22 @@ export class WidgetsComponent implements OnInit {
     // Generally, we would want to wait for the result of `widgetsService.deleteWidget`
     // before resetting the current widget.
     this.resetWidget();
+  }
+
+  unsetFeaturedWidget() {
+    const featured = this.widgets.find(widget => widget.featured);
+
+    if (featured) {
+      this.saveWidget(Object.assign({}, featured, {featured: false}));
+    }
+  }
+
+  setWidgetAsFeatured(widget: Widget) {
+    this.unsetFeaturedWidget();
+
+    this.saveWidget(Object.assign({}, widget, {featured: true}));
+
+    this.router.navigate(['featured', widget.id], {relativeTo: this.route});
   }
 
 }
